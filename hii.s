dba38975d4cc67d36f73c3644ec7159daa7f1384
simran1915 <69317580+simran1915@users.noreply.github.com>
2020-08-07 16:21:16 +0530

    created hii.s
    This file prints hii world on the screen.

---
+++ hii.s
@@ -0,0 +1,8 @@
+# Python proogram to print positive 
+# List of numbers
+list = [ 11 , -21 , 0 ,45 ,66 , -93 ]
+#iterating each number in list
+for num in list:
+     # checking condition 
+      if num >= 0:
+			   print (num, end = ' ')
